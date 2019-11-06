9fe732deeef34a4544f24bc90ddd050d8dc6e306
Andrei Caminschi <andrei.caminschi@scriptics.ro>
2019-11-06 14:07:16 +0200

    Updated signature of BelongsTo decorator Removed HasManyThrough decorator

--- src/decorators.ts
+++ src/decorators.ts
@@ -46,21 +46,22 @@ export function HasOne(options?: RelationOptions) {
     }
 }
 
-export function BelongsTo(local_key?: string, foreign_key?: string) {
+export function BelongsTo(options?: RelationOptions) {
     return function (target: Model, key: string, descriptor: PropertyDescriptor) {
         target.RecordInfo.Relations[key] = 'belongs-to';
 
         let original_value = descriptor.value;
         descriptor.enumerable = false;
         descriptor.value = function (this: Model) {
+            if (!options) options = {};
             let r = this.GetRelationValue(key);
             if (!r) {
                 r = original_value();
                 if (!Object.isModel(r)) throw "Return value of a method decorated as a BelongsTo relation must be an instance of a model";
                 this.SetRelationValue(key, r);
             }
-            let lk = local_key || r.RecordInfo.Name + '_id';
-            let fk = foreign_key || 'id';
+            let lk = options.LocalKey || 'id';
+            let fk = options.ForeignKey || this.$name + '_id';
             r.SetAttribute(fk.snakeCaseToCamelCase(), this.GetAttribute(lk.snakeCaseToCamelCase()));
             return r;
 
@@ -98,33 +99,6 @@ export function HasMany(options?: RelationOptions) {
                 };
             }
 
-            return r;
-
-        };
-        return descriptor;
-    }
-}
-
-export function HasManyThrough(type: string, local_key?: string, foreign_key?: string) {
-    return function (target: Model, key: string, descriptor: PropertyDescriptor) {
-        target.RecordInfo.Relations[key] = 'has-many-though';
-        let original_value = descriptor.value;
-        descriptor.enumerable = false;
-
-        descriptor.value = function (this: Model) {
-            let r: Repository<Model> = this.GetRelationValue(key);
-            if (!r) {
-                r = original_value();
-                if (!Object.isRepository(r)) throw "Return value of a method decorated as a HasMany relation must be an instance of a repository";
-                r.WhereEquals('type', type, true);
-                this.SetRelationValue(key, r);
-            }
-
-            let lk = local_key || 'id';
-            let fk = foreign_key || this.RecordInfo.Name + '_id';
-            r.WhereEquals(fk, this.GetAttribute(lk.snakeCaseToCamelCase()), true);
-            r.AddModelPersistentAttribute(fk, this.GetAttribute(lk.snakeCaseToCamelCase()));
-            r.AddModelPersistentAttribute('type', type);
             return r;
 
         };
